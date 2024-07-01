34bf8c36c11a694b38e4f8cfbfb36dd522b32857
Plumnet <tomomi0131@ymail.ne.jp>
2024-07-02 08:34:15 +0900

    firestoreのデータを表示させようと試みた(失敗4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,101 +2,64 @@ import { Box, Button, ChakraProvider, Flex, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 // import db from "@/lib/firebase/firebase";
-import { QuerySnapshot, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import { QuerySnapshot, collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
 import db from '@/firebase';
-
 
 export default function index() {
   const [posts, setPosts] = useState([]);
-
   const initialState = ['id: 1', 'id: 2'];
-
   const [taskId, setTaskId] = useState(initialState);
 
-  const [messages, setMessages] = useState([])
-
-  // async function todo() {
-  //   const col = collection(db, "todo");
-  //   const q = query(col, where("id", "==", taskId));
-  //   const task: any = await getDocs(q).then((snapshot) => {
-  //     return snapshot.docs[0].data();
-  //   });
-  //   console.log(task)
-  // }
-
-  // async function todo() {
-  //   const tasks = await getDocs(collection(db, "todo")).then((snapshot) =>
-  //     snapshot.docs.map((doc) => {
-  //       return doc.data();
-  //     })
-  //   );
-  //   console.log(tasks)
-  // }
-
-  // todo()
-
-  //ReactでLINEクローンの作り方 - React×Firebaseチュートリ//ReactでLINEクローンの作り方 - React×Firebaseチュートリアルの34分以降
-  useEffect(() => {
-    //何故かcollectionが'Firestore' に存在しません。と出てくる
-    db.collection("todo")
-      .limit(50)
-      .onSnapshot((snapshot: any) => {
-        setMessages(snapshot.docs.map((doc: any) => doc.data()))
-      })
-  }, []);
+  //ReactでLINEクローンの作り方 - React×Firebaseチュートリ//ReactでLINEクローンの作り方 - React×Firebaseチュートリアルの38:20の時点のコード
+  function Line() {
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+      //エラー発生箇所、"プロパティ 'collection' は型 'Firestore' に存在しません。"
+      db.collection("messages")
+        .limit(50)
+        .onSnapshot((snapshot: any) => {
+          setMessages(snapshot.docs.map((doc: any) => doc.data()))
+        })
+    }, []);
 
 
-
-
-  const innerBoxStyles = {
-    // boxSize: '150px',
-    p: '5',
-    backgroundImage:
-      'url(https://livedoor.blogimg.jp/zeropasoakita/imgs/e/5/e555c3b1-s.jpg) ',
-  }
-
-
-
-  return (
-    <div>
-      <ChakraProvider>
-        {/* <Box sx={innerBoxStyles}>
-          <Text fontSize={32} color='RED' textAlign={['left']}>
-            ホーム画面
-          </Text>
-          doc.data()
-        </Box>
-        <Flex align="center" justify="center" padding={200}>
-          <Link href={"/list"}>
-            <Text fontSize={32}>
-              一覧画面へ
-            </Text>
-          </Link>
-        </Flex> */}
+    return (
+      <div>
+        //エラー発生箇所、"型 'void' を型 'ReactNode' に割り当てることはできません。"
+        {console.log(messages)}
         <div className="msgs">
-          //ここでエラーになる
-          {messages.map(({ id, text, photoURL, uid }) => {
-            <div>
-              <div key={id}>
-                <img src={photoURL} alt="" />
-                <p>{text}</p>
-              </div>
-            </div>
-          })}
+          {messages.map(({ id, text }))}
+          <div key={id}>
+            <p>{text}</p>
+          </div>
         </div>
-      </ChakraProvider>
-        //https://qiita.com/masakiwakabayashi/items/8d33f6df1a7ec4dbfa3a/参考記事
-      {/* {
-            // map関数を使ってstaffのデータを表示
-            tasks.map((task: any) => {
-              return (
-                <Box key={task.id}>
-                  <Flex>
-                    <Box p={3} minWidth={'73px'}>
-                      {task.name}
-                    </Box>
-                  </ChakraProvider>
-                </div>
-              )
-            }
-        } */}
+      </div>
+    )
+  }
+  // <div>
+  //   <ChakraProvider>
+  //     <Box sx={innerBoxStyles}>
+  //       <Text fontSize={32} color='RED' textAlign={['left']}>
+  //         ホーム画面
+  //       </Text>
+
+  //     </Box>
+  //     <Flex align="center" justify="center" padding={200}>
+  //       <Link href={"/list"}>
+  //         <Text fontSize={32}>
+  //           一覧画面へ
+  //         </Text>
+  //       </Link>
+  //     </Flex>
+  //   //https://qiita.com/masakiwakabayashi/items/8d33f6df1a7ec4dbfa3a/参考記事
+  //     <div className="msgs">
+  //       {messages.map(({ id, text }) => (
+  //         <div>
+  //           <div key={id}>
+  //             <p>{text}</p>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+
+}
